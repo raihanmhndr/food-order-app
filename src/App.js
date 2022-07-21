2d9ac58be0b4b115b9b4f3adea48634361b31c07
raihan <mahendraihan@gmail.com>
2022-07-21 22:52:27 +0700

    fix: nav link page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 // import logo from './logo.svg';
 import './App.css';
 import NavigationBar from './components/NavBar';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Footer from './components/Footer';
+import Menu from './pages/Menu';
 
 function App() {
   return (
     <Router>
-      <NavigationBar/>
+      <NavigationBar />
       <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/catalogue' element={<Home />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/catalogue" element={<Menu />}></Route>
       </Routes>
       <Footer />
     </Router>
